2c97d641e2414f0b581200f5272772f370405d07
Arthur Wu <arthur.kainan.wu@gmail.com>
2024-01-10 01:21:42 -0500

    changed toggle button to a switch

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from '/vite.svg'
 import './styling/App.css'
 import Deck from './components/Deck'
 import Input from './components/Input'
+import Switch from '@mui/material/Switch';
 
 function App() {
 
@@ -18,8 +19,8 @@ function App() {
       <h2>Arthur's Flashcards App</h2>
       <h4>Powered by ChatGPT</h4>
 
-      <button onClick={() => {setAiOn(!aiOn)}}>
-        {aiOn ? "Turn AI Mode Off" : "Turn AI Mode On"}</button>
+      {aiOn ? "Toggle AI Mode Off":"Toggle AI Mode On"}
+      <Switch onChange={() => {setAiOn(!aiOn)}}></Switch>
       
       <Deck cards={cards}></Deck>
       <Input cardSetter={setCards} currDeck={cards} aiToggle={aiOn}></Input>
